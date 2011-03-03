14d71f9da00ff39707779d20a69ef9fb3709ff6c
Jan Křička <krica@gravastar.cz>
2011-03-03 22:54:43 +0100

    photos migration

--- db/migrate/20101014221157_add_photo_attribute.rb
+++ db/migrate/20101014221157_add_photo_attribute.rb
@@ -1,7 +1,7 @@
 class AddPhotoAttribute < ActiveRecord::Migration
   def self.up
-  remove_column :photos, :image_updated_at
-	remove_column :photos, :description
+  #remove_column :photos, :image_updated_at
+	#remove_column :photos, :description
     add_column :photos, :image_updated_at, :datetime
 	add_column :photos, :description, :string
     change_column :photos, :image_file_size, :integer
